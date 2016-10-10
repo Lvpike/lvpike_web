ac18e74f05ce93b9140bad98dd73fadff2498cc9
Lvpike <949715925@qq.com>
2016-10-10 18:32:45 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
+    $("p").dblclick(function(){
+    $(this).hide();//当双击元素时，会发生 dblclick 事件。
+    });
+    $("#p1").mouseenter(function(){
+        alert("You entered p1!");
+    });//当鼠标指针穿过元素时，会发生 mouseenter 事件。
+    $("#p1").mouseleave(function(){
+        alert("Bye! You now leave p1!");
+    });
+});
